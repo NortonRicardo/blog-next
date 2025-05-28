7decb4937e0b67c2de24a09d3cd9069c7967cbc3
Norton Ricardo <nortonricardo@live.com>
2025-05-28 16:25:14 -0300

    feat:  layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,20 @@
-import { Header } from '@/components/header/header'
+// eslint-disable-next-line camelcase
+import { Geist, Geist_Mono } from 'next/font/google'
 
-// import { Geist, Geist_Mono } from 'next/font/google'
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+})
 
-// const geistSans = Geist({
-//   variable: '--font-geist-sans',
-//   subsets: ['latin'],
-// })
-
-// const geistMono = Geist_Mono({
-//   variable: '--font-geist-mono',
-//   subsets: ['latin'],
-// })
-// className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+})
+// grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20
 export default function Home() {
   return (
-    <div>
-      <Header />
-    </div>
+    <div
+      className={`${geistSans.className} ${geistMono.className} font-[family-name:var(--font-geist-sans)]`}
+    ></div>
   )
 }
